8da812d90e4590b243c06c4a42338a1c7140af97
Nikita Migunov <NAMigunov@sberbank.ru>
2024-08-11 17:46:23 +0300

    remove extra annotation

--- src/main/java/com/example/persistence/examples/model/db/Author.java
+++ src/main/java/com/example/persistence/examples/model/db/Author.java
@@ -2,7 +2,6 @@ package com.example.persistence.examples.model.db;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -25,7 +24,6 @@ public class Author {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "author_id")
     private Long id;
 
     private String name;
