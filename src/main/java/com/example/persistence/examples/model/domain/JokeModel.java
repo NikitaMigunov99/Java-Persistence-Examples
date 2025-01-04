54fb25c2280c7c75439d278a82af3d8421f35332
Nikita Migunov <NAMigunov@sberbank.ru>
2025-01-04 14:17:59 +0300

    fix equals and hashCode

--- src/main/java/com/example/persistence/examples/model/domain/JokeModel.java
+++ src/main/java/com/example/persistence/examples/model/domain/JokeModel.java
@@ -1,5 +1,7 @@
 package com.example.persistence.examples.model.domain;
 
+import java.util.Objects;
+
 /**
  * Model of joke
  */
@@ -8,4 +10,25 @@ public record JokeModel(
         String type,
         String setup,
         String punchline) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof JokeModel jokeModel)) return false;
+        return type.equals(jokeModel.type) && setup.equals(jokeModel.setup) && punchline.equals(jokeModel.punchline);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, setup, punchline);
+    }
+
+    @Override
+    public String toString() {
+        return "JokeModel{" +
+                "type='" + type + '\'' +
+                ", setup='" + setup + '\'' +
+                ", punchline='" + punchline + '\'' +
+                '}';
+    }
 }
